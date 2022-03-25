cc18971aca30a5be685a95af11da949e88ab2cb3
Leandro Santos <lsaantos@outlook.com>
2022-03-25 10:56:09 -0300

    add dependences

--- src/components/CreateRoom/index.tsx
+++ src/components/CreateRoom/index.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from 'next/router';
 // import { v1 as uuid } from "uuid";
 
 export function CreateRoom () {
-    function create() {
-        // const id = uuid();
-        // props.history.push(`/room/${id}`);
+    const [groupName, setGroupName] = useState('');
+    const router = useRouter();
+
+
+    function joinOnMeet() {
+        if(!groupName) return;
+        router.push(`/room/${groupName}`);
     }
 
     return (
-        <button onClick={create}>Create room</button>
+        <form style={{ display: 'flex', flexDirection: 'column', gap: '16px'}}>
+            <input style={{}} value={groupName} onChange={(e)=> setGroupName(e.target.value)}></input>
+            <button onClick={joinOnMeet}>Entrar no Grupo!</button>
+        </form>
     );
 };
 
