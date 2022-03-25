125b928dd65e1f30be825543cd44c207b6edf8ff
Leandro Santos <lsaantos@outlook.com>
2022-03-25 10:41:58 -0300

    initial pages

---
+++ pages/room/[id].tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Room: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Room</title>
+      </Head>
+
+      <main>
+       <Room />
+      </main>
+    </div>
+  )
+}
+
+export default Room;
