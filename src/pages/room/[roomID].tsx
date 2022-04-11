294e011b3b8a639c9d0bfe69fae393c94870866a
João Luiz <jleaopitta@gmail.com>
2022-04-11 17:18:40 -0300

    refactor: move pages and styles into src

---
+++ src/pages/room/[roomID].tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import { Room as RoomComponent } from "../../components/Room";
+import Head from "next/head";
+
+const Room: NextPage = () => {
+  return <RoomComponent />;
+};
+
+export default Room;
