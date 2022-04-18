284b8763c574012c6974e047b6dd56a88453f3de
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2022-04-18 14:41:33 -0300

    chore: change heroku cli

--- src/components/Room/index.tsx
+++ src/components/Room/index.tsx
@@ -50,12 +50,12 @@ export function Room() {
   const { roomID: room_id } = router.query;
   const roomID = room_id;
 
-  // const socket = useMemo(
-  //   () => io("https://strawberry-pudding-43161.herokuapp.com/"),
-  //   []
-  // );
+  const socket = useMemo(
+    () => io("https://strawberry-pudding-43161.herokuapp.com/"),
+    []
+  );
 
-  const socket = useMemo(() => io("http://localhost:8000/"), []);
+  // const socket = useMemo(() => io("http://localhost:8000/"), []);
 
   const disconnectRoom = () => {
     socket.disconnect();
